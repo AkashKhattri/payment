21dc351ab7f1d9a94a039b685350aa0f0e72ecc9
Akash-Khattri <akash.khattri.ak@gmail.com>
2020-03-03 11:52:45 +0545

    payment module

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
+import chair from './Chair.jpg';
+import gif from './giphy.webp';
+
+function Product({ product }) {
+  const [paidFor, setPaidFor] = useState(false);
+  const [error, setError] = useState(null);
+  const paypalRef = useRef();
+
+  useEffect(() => {
+    window.paypal
+      .Buttons({
+        createOrder: (data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                description: product.description,
+                amount: {
+                  currency_code: 'USD',
+                  value: product.price
+                }
+              }
+            ]
+          });
+        },
+        onApprove: async (data, actions) => {
+          const order = await actions.order.capture();
+          setPaidFor(true);
+          console.log(order);
+        },
+        onError: err => {
+          setError(err);
+          console.error(err);
+        }
+      })
+      .render(paypalRef.current);
+  }, [product.description, product.price]);
+
+  if (paidFor) {
+    return (
+      <div>
+        <h1>Congrats, you just bought {product.name}!</h1>
+        <img src={gif} alt={product.description} />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {error && <div>Uh oh, an error occurred! {error.message}</div>}
+      <h1>
+        {product.description} for ${product.price}
+      </h1>
+      <img src={product.image} alt={product.description} width='200' />
+      <div ref={paypalRef} />
+    </div>
+  );
+}
 
 function App() {
+  const product = {
+    price: 777.77,
+    name: 'comfy chair',
+    description: 'fancy chair, like new',
+    image: chair
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Product product={product} />
     </div>
   );
 }
